8a1e323a211c94bfa2dd0bc364e61206f72e4789
Sua <perezenderson494@gmail.com>
2024-04-23 09:13:56 -0600

    Ojalá me atropelle un camioncito kun
    Ojalá me atropelle un camioncito kun

--- agencia_viajes/lib/screen/hoteles_screen.dart
+++ agencia_viajes/lib/screen/hoteles_screen.dart
@@ -7,6 +7,8 @@ import 'package:agencia_viajes/models/pais.dart';
 import 'package:agencia_viajes/models/place.dart';
 import 'package:agencia_viajes/models/profile.dart';
 import 'package:agencia_viajes/screen/hotel_screen.dart';
+import 'package:agencia_viajes/screen/layout.dart';
+import 'package:agencia_viajes/screen/transportes.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -544,13 +546,61 @@ class _HotelExpansionTileState extends State<HotelExpansionTile> {
           },
           trailing: IconButton(
             icon: Icon(
-              isExpanded ? Icons.expand_less : Icons.expand_more,
+              isExpanded ? Icons.arrow_right : Icons.arrow_right,
               color: Colors.white,
             ),
             onPressed: () {
-              setState(() {
-                isExpanded = !isExpanded;
-              });
+              print("AAAAAAAAAA");
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => HotelScreen(
+                    hotel: Hotel(
+                        hoteId: 1,
+                        hoteNombre: "Hotel",
+                        hoteDireccionExacta: "Direccion",
+                        hotePrecioTodoIncluido: 200,
+                        haHoPrecioPorNoche: 500,
+                        hoteEstrellas: 5,
+                        ciudId: "ciudId",
+                        ciudDescripcion: "DescripcionCiud",
+                        estaDescripcion: "DescripcionEsta",
+                        hoteEstado: 1,
+                        hoteFechaCreacion: "FechaCreacion",
+                        hoteHoraSalida: "HoraSalida",
+                        hoteImagen:
+                            "https://cdn2.thecatapi.com/images/b9r.jpg",
+                        hoteUsuaCreacion: 1,
+                        paisDescripcion: "pais"),
+                    place: const Place(
+                        address: "adress",
+                        bathCount: 5,
+                        bedCount: 5,
+                        bedroomCount: 5,
+                        city: "city",
+                        costPerNight: 500,
+                        country: "country",
+                        guestCount: 50,
+                        imageUrls: [
+                          "https://cdn2.thecatapi.com/images/3ql.jpg",
+                          "https://cdn2.thecatapi.com/images/9p2.jpg",
+                          "https://cdn2.thecatapi.com/images/b9r.jpg"
+                        ],
+                        numberOfRatings: 500,
+                        owner: Profile(
+                          isSuperhost: true,
+                          name: "owner",
+                          profileImageUrl: "profileImageUrl",
+                        ),
+                        rating: 5,
+                        state: "state",
+                        title: "title",
+                        type: PlaceType.apartment,
+                        zipcode: "zipcode",
+                        description: "description"),
+                  ),
+                )
+              );
             },
           ),
           children: [
